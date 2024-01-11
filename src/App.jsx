8bfb8cf1501eb51dc0533bb4c5d52bb3cdbaff27
awatefaissaoui <awatef.aissaoui23@gmail.com>
2024-01-11 16:42:05 +0100

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,37 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import Liste from './components/List'
-import Navbar from './components/Navbar'
-import Fromename from './components/Frome'
-import Filter from './components/Filter'
+import { useState, useEffect } from "react";
+import "./App.css";
+import Liste from "./components/List";
+import Navbar from "./components/Navbar";
+import Fromename from "./components/Frome";
+import Filter from "./components/Filter";
 function App() {
   const [movies, setmovies] = useState([]);
-  const [filtred, setFiltred] = useState([])
-
-
+  const [filtred, setFiltred] = useState([]);
 
   async function get_data() {
     const response = await fetch("http://localhost:3000/movies");
     const data = await response.json();
-    setmovies(data)
+    setmovies(data);
   }
 
-
   useEffect(() => {
-    get_data()
+    get_data();
   }, []);
 
   return (
-
     <>
       <Navbar />
 
-      {window.location.pathname == "/"  && <> <Filter setFiltred={setFiltred} movies={movies} /><Liste movies={filtred.length == 0 ? movies : filtred} /></>}
+      {window.location.pathname == "/" && (
+        <>
+          {" "}
+          <Filter setFiltred={setFiltred} movies={movies} />
+          <Liste movies={filtred.length == 0 ? movies : filtred} />
+        </>
+      )}
       {window.location.pathname == "/new" && <Fromename />}
-
-
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
